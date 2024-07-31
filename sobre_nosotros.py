4457cd2fbb4ef314a17a04698ea1c10f7a64c5de
juanxcl98 <juanporcel98@gmail.com>
2024-07-31 18:00:31 -0300

    Update sobre_nosotros.py

--- sobre_nosotros.py
+++ sobre_nosotros.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def mostrar_sobre_nosotros():
     st.title("Sobre Nosotros")
-    st.write("Somos una empresa dedicada a...")
+    st.write("Somos una empresa dedicada a la Discapacidad en Niñez y Adolescencia⁣")
 
 
 
