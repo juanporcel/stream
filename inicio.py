8ec88014b9f2a4d0ae25fd8c1c8e61577b009743
juanxcl98 <juanporcel98@gmail.com>
2024-07-31 17:32:49 -0300

    Update inicio.py

--- inicio.py
+++ inicio.py
@@ -6,7 +6,7 @@ def mostrar_inicio():
     st.title('Inicio')
 
     # Imagen desde una URL
-    url_imagen = "https://cdn.prod.website-files.com/634fa785d369cb60d80b6dd1/6393298e18f50e62a1657530_ETL%20process%20DataChannel-p-1080.webp"  # Reemplaza con la URL de tu imagen
+    url_imagen = "img/cai.jpg"  # Reemplaza con la URL de tu imagen
     st.image(url_imagen, use_column_width=True)
 
    
