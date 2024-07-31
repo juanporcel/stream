23bfa854264ec11eed6b02fbb655bf27807aea18
juanxcl98 <juanporcel98@gmail.com>
2024-07-31 18:04:42 -0300

    Update data_analytics.py

--- data_analytics.py
+++ data_analytics.py
@@ -15,3 +15,25 @@ def mostrar_data():
     # Mostrar el iframe en la aplicación
     components.html(iframe_code, height=400)
 
+    st.write("Transporte")
+
+    # Código del iframe
+    iframe_code = """
+    <iframe title="transporte" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiY2I0ZWQ1ZGQtNDBhOC00NDljLWI4NmYtZmFkY2M5MTcyYzk0IiwidCI6ImRiODM5ZTQ4LWE0YzYtNDU5ZC1iOWMwLTZmNzI5M2RiOGYzYiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    """
+
+    # Mostrar el iframe en la aplicación
+    components.html(iframe_code, height=400)
+
+    st.write("Grilla de pacientes")
+
+    # Código del iframe
+    iframe_code = """
+    <iframe title="grillapacientes" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiODE4YmUxOTMtMjg5ZS00OWM4LThiNDYtMzkxYzM2MGY5YWYyIiwidCI6ImRiODM5ZTQ4LWE0YzYtNDU5ZC1iOWMwLTZmNzI5M2RiOGYzYiIsImMiOjR9" frameborder="0" allowFullScreen="true"></iframe>
+    """
+
+    # Mostrar el iframe en la aplicación
+    components.html(iframe_code, height=400)
+
+
+
