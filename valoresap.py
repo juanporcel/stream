a95c0201a10a2b4fb98c9c6baf98ebc7e2710bb8
juanxcl98 <juanporcel98@gmail.com>
2024-08-01 08:51:31 -0300

    Update valoresap.py

--- valoresap.py
+++ valoresap.py
@@ -2,12 +2,12 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def mostrar_cuadro_valores():
-
     # Código del iframe con estilos CSS para adaptabilidad
     iframe_code_valores = """
     <div style="display: flex; justify-content: center;">
-        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSlH7Y6NTXvhkpH9BMoY3Q2mvdijJ0_sDmmOFzNiQip0iwd4xyPd46TGZvvgufw17xelQ6iobLJ5ySK/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
+        <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSlH7Y6NTXvhkpH9BMoY3Q2mvdijJ0_sDmmOFzNiQip0iwd4xyPd46TGZvvgufw17xelQ6iobLJ5ySK/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"
+        width="100%" height="600"></iframe>
     </div>
     """
-    components.html(iframe_code_valores, height=300)
+    components.html(iframe_code_valores, height=600)
     
